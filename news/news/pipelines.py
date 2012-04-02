c2264499b47583b94102929142e422f3fac553ed
jaimin patel <jaimin9999@gmail.com>
2012-04-01 22:14:20 -0400

    Read URLs from db and run the feed parser, if find the match insert it into db

--- news/news/pipelines.py
+++ news/news/pipelines.py
@@ -1,5 +1,7 @@
 #define your item pipelines here
 from scrapy.exceptions import DropItem
+from xml.dom.minidom import parseString
+#import xml.parser.expat
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/topics/item-pipeline.html
 import MySQLdb
@@ -11,18 +13,14 @@ db = MySQLdb.connect("localhost","root","local","MyDB")
 cursor = db.cursor()
 
 class newsPipeline(object):
-    def process_item(self, item, spider):		  
-	for a in item['title']: 			# Pick each item from the list
- 		a1=item.get('title')[0]		# Get the zero-index value for the 'title' key
-            	a2=item.get('link')[0]		# Get the zero-index value for the 'link' key
-           	a3=item.get('desc')[0]		# Get the zero-index value for the 'desc' key
-            	b1=a1[7:-15]
-           	b2=a2[6:-14]
-            	b3=a3[13:-29]
-            	del item['title']
-            	item['title']=b1			# New edited title values assigned
-            	item['link']=b2
-           	item['desc']=b3
+    def process_item(self, xmlitem, spider):
+	print 'xmlitem: ', xmlitem	
+	item = {}  
+	for a in xmlitem['title']: 			# Pick each item from the list
+		item['title'] = parseString(xmlitem.get('title')[0]).firstChild.firstChild.toxml()
+		item['link'] = parseString(xmlitem.get('link')[0]).firstChild.firstChild.toxml()
+		item['desc'] = parseString(xmlitem.get('desc')[0]).firstChild.firstChild.toxml()
+		#print 'a1 :', item['title'], 'a2 :', item['link'], 'a3 :', item['desc']
 	filter = ['Japan', 'japan', 'Japanese', 'japanese', 'earthquake', 'Earthquake', 'tsunami', 'Tsunami'] 
 	for x in filter:
 		for s,t in item.iteritems():
@@ -47,32 +45,3 @@ class newsPipeline(object):
 				raise DropItem ("Not correct")
 	#disconnect from the server
 	db.close()
-'''
-class WsjPipeline(object):
-	def process_item(self, item, spider):
-		a1 = item.get('title')[1]
-		a2 = item.get('link')[1]
-		a3 = item.get('desc')[1]
-		for t in item.iteritems():
-			b1=a1[7:-15]
-			b2=a2[6:-14]
-			b3=a3[13:-29]
-			del item['title']
-			item['title']=b1
-			item['link']=b2
-			item['desc']=b3
-		return item
-class WsjPipeline2(object):
-	def process_item(self, item, spider):
-		for a in item['title']:
-			a1 = item.get('title')
-			a2 = item.get('link')
-			a3 = item.get('desc')
-			for t in item.iteritems():
-				b1 = a1 + 'tuvalabs'
-				b2 = a2
-				b3 = a3
-				del item['title']
-				ite['title']=b1
-			return item
-'''
--- news/news/spiders/spider.py
+++ news/news/spiders/spider.py
@@ -9,29 +9,32 @@ db = MySQLdb.connect("localhost","root","local","MyDB")
 #prepare a cursor object using cursor() method
 cursor = db.cursor()
 
-sql = "select select url from sourceurls where deleted_ind is null limit 10"
+sql = "select url from MyDB.SourceUrls where deleted_ind is null limit 10"
 arr = []
 
 try:
 	cursor.execute(sql)
 	result = cursor.fetchall()
-	print result
+	#print result
 	for row in result:
-		arr.append(row)	
+		arr.extend(row)	
 except:
 	print "Error: unable to fetch the data"
 
 db.close()
 
-spider1(XMLFeedSpider):
+#use XmlPathSelector (other one is HtmlXPathSelector - for HTML data)
+class spider1(XMLFeedSpider):
 	name='newsspider'
-	start_urls = ['http://feeds.mercurynews.com/mngi/rss/CustomR']
+	#start_urls = ['http://feeds.mercurynews.com/mngi/rss/CustomR']
+	start_urls = arr
 	itertag = 'item'
 	
 	def parse_node(self, response, node):
 		item = newsItem()
 		if node is None :
 			return None
+		#extract() - returns a unicode string with the data selected by the XPath selector.
 		item['title'] = node.select('title').extract()
 		item['desc'] = node.select('description').extract()
 		item['link'] = node.select('link').extract()
